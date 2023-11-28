1808c6691a8fcc418af90d16182cf46c25b807a9
Joshua <hola@joshua.cl>
2023-11-28 14:50:49 -0300

    Middleware para poder usar Date en el Store

--- src/store/store.js
+++ src/store/store.js
@@ -5,5 +5,9 @@ export const store = configureStore({
   reducer: {
     calendar: calendarSlice.reducer,
     ui: uiSlice.reducer
-  }
+  },
+  // Configurando middelware para que permita insertar funciones de tipo date en el store sin mayores inconvenientes
+  middleware: ( getDefaultMiddlewate ) => getDefaultMiddlewate({
+    serializableCheck: false
+  })
 });
