f17439e2121bcb6ae60aeb6d637a60f11afdd73f
Joshua <hola@joshua.cl>
2023-12-21 13:56:15 -0300

    Configurando reducers en 'authSlice'

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 export * from './ui/uiSlice';
 export * from './calendar/calendarSlice';
+export * from './auth/authSlice';
 
 // Para usar en el "Store" los reducers creados en los "slices", importar el "Store" despues de esos reducers
 export * from './store';
