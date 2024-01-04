6b6ee8fbe8ff0b71457fff4a6da11f1e8c77ad8e
Joshua <hola@joshua.cl>
2024-01-04 18:34:36 -0300

    Testing authSlice y creacion de fixtures

---
+++ tests/fixtures/testUser.js
@@ -0,0 +1,7 @@
+// Usuario para los testing, este usuario debe estar creado en la base de datos
+export const testUserCredentials = {
+  "email": "test@correo.cl",
+  "password": "123456",
+  "uid": "6596c3a1993c2e03d550130e",
+  "name": "Test User",
+}
