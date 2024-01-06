cbebd33a0e1c8eb180e268022edb84c90ea9f21d
Joshua <hola@joshua.cl>
2024-01-06 15:53:57 -0300

    Testing startLogin error en useAuthStore

--- tests/hooks/useAuthStore.test.js
+++ tests/hooks/useAuthStore.test.js
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { authSlice } from '../../src/store';
-import { act, renderHook } from '@testing-library/react';
+import { act, renderHook, waitFor } from '@testing-library/react';
 import { useAuthStore } from '../../src/hooks/useAuthStore';
 import { Provider } from 'react-redux';
 import { initialState, notAuthenticatedState } from '../fixtures/authStates';
@@ -63,6 +63,36 @@ describe('Pruebas en el useAuthStore', () => {
 
     expect( localStorage.getItem( 'token' ) ).toEqual( expect.any( String ) );
     expect( localStorage.getItem( 'token-init-date' ) ).toEqual( expect.any( String ) );
+
+  })
+
+  test('startLogin debe de fallar la autenticacion', async () => {
+
+    localStorage.clear();
+    const mockStore = getMockStore({ ...notAuthenticatedState });
+
+    const { result } = renderHook( () => useAuthStore(), {
+      wrapper: ({ children }) => <Provider store={ mockStore }>{ children }</Provider>
+    });
+
+    // Usuario falso para forzar la no autenticacion
+    await act( async () => {
+      await result.current.startLogin({ email: 'test@example.com', password: 'abcxyz'});
+    });
+
+    const { errorMessage, status, user } = result.current;
+
+    expect( localStorage.getItem('token') ).toBe( null );
+
+    expect({ errorMessage, status, user }).toEqual({
+      errorMessage: 'Credenciales incorrectas',
+      status: 'not-authenticated',
+      user: {}
+    })
+
+    await waitFor(
+      () => expect( result.current.errorMessage ).toBe( undefined )
+    );
 
   })
 
